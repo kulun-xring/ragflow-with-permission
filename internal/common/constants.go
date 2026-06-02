3c6f3c05dd3f9ea2c1fcea4d576913ccc51d2615
root <root@oa-kulun.localdomain>
2026-06-02 11:12:36 +0800

    renew

--- internal/common/constants.go
+++
@@ -1,8 +0,0 @@
-package common
-
-const (
-	// PAGERANK_FLD is the field name for pagerank score
-	PAGERANK_FLD = "pagerank_fea"
-	// TAG_FLD is the field name for tag features
-	TAG_FLD = "tag_feas"
-)
